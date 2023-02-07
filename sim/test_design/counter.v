8dd4837a4f536e7b6e4f56a58d9cc5d453121a94
gatecat <gatecat@ds0.me>
2023-02-07 15:11:47 +0100

    IO enable is inverted
    Signed-off-by: gatecat <gatecat@ds0.me>

--- sim/test_design/counter.v
+++ sim/test_design/counter.v
@@ -9,5 +9,5 @@ module top(input wire clk, input wire [30:0] io_in, output wire [30:0] io_out, i
 			ctr <= ctr + 1'b1;
 
 	assign io_out = {13'h123, ctr, rst, 1'b0}; // pass thru reset for debugging
-	assign io_oeb = 28'b1;
+	assign io_oeb = ~(28'b1);
 endmodule
