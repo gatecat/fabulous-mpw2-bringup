ba4ba8aea6447c674dbb85e693a4529bc105b2a2
gatecat <gatecat@ds0.me>
2023-04-03 11:50:28 +0200

    add io pattern for test board
    Signed-off-by: gatecat <gatecat@ds0.me>

---
+++ fuserisc/caravel_nucelo/gpio_config_def.py
@@ -0,0 +1,55 @@
+# gpio_config_def.py file for part 0
+# io_config -- version 1.1.0
+
+H_NONE        = 0  
+H_DEPENDENT   = 1  
+H_INDEPENDENT = 2  
+H_SPECIAL     = 3  
+H_UNKNOWN     = 4  
+
+# voltage: 1.6
+# IO configuration chain was successful
+gpio_l = [
+['IO[0]', H_NONE],
+['IO[1]', H_INDEPENDENT],
+['IO[2]', H_DEPENDENT],
+['IO[3]', H_DEPENDENT],
+['IO[4]', H_DEPENDENT],
+['IO[5]', H_DEPENDENT],
+['IO[6]', H_INDEPENDENT],
+['IO[7]', H_INDEPENDENT],
+['IO[8]', H_INDEPENDENT],
+['IO[9]', H_DEPENDENT],
+['IO[10]', H_INDEPENDENT],
+['IO[11]', H_INDEPENDENT],
+['IO[12]', H_INDEPENDENT],
+['IO[13]', H_INDEPENDENT],
+['IO[14]', H_INDEPENDENT],
+['IO[15]', H_INDEPENDENT],
+['IO[16]', H_INDEPENDENT],
+['IO[17]', H_INDEPENDENT],
+['IO[18]', H_INDEPENDENT],
+]
+# voltage: 1.6
+# IO configuration chain was successful
+gpio_h = [
+['IO[37]', H_NONE],
+['IO[36]', H_INDEPENDENT],
+['IO[35]', H_INDEPENDENT],
+['IO[34]', H_INDEPENDENT],
+['IO[33]', H_DEPENDENT],
+['IO[32]', H_DEPENDENT],
+['IO[31]', H_INDEPENDENT],
+['IO[30]', H_INDEPENDENT],
+['IO[29]', H_INDEPENDENT],
+['IO[28]', H_DEPENDENT],
+['IO[27]', H_INDEPENDENT],
+['IO[26]', H_DEPENDENT],
+['IO[25]', H_INDEPENDENT],
+['IO[24]', H_INDEPENDENT],
+['IO[23]', H_INDEPENDENT],
+['IO[22]', H_INDEPENDENT],
+['IO[21]', H_INDEPENDENT],
+['IO[20]', H_INDEPENDENT],
+['IO[19]', H_INDEPENDENT],
+]
